fc77f85785291642b12547353ad47383f0bb6b1a
Bim <omegatoy@Jiradets-MacBook-Pro.local>
2020-09-03 15:53:22 +0700

    Getting started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Bim on 9/3/20.
+//  Copyright © 2020 Jiradet Amornpimonkul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
