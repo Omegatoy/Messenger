6dbb6500f33f3dc82cd70bcf43dadc0f2c0ebce6
Bim <omegatoy@Jiradets-MacBook-Pro.local>
2020-09-03 16:05:00 +0700

    Wrap up getting started

---
+++ Messenger/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewerViewController.swift
+//  Messenger
+//
+//  Created by Bim on 9/3/20.
+//  Copyright © 2020 Jiradet Amornpimonkul. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
